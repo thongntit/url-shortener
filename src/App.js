e548f19b489b5f3e19fb33ffa3e3377adcb505ea
Thong Nguyen <nguyentrthong95@gmail.com>
2019-12-15 01:34:48 +0700

    Parse url

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
-import { Typography, Button, Row, Col } from "antd";
+import { Typography, Button, Row, Col, Input } from "antd";
 import * as firebase from "firebase";
 import { firebaseConfig } from "./config";
-const { Text } = Typography;
+const { Title, Text } = Typography;
 
 firebase.initializeApp(firebaseConfig);
 
@@ -11,14 +11,10 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      redirect: false
+      url: ""
     };
   }
-  onRedirect = () => {
-    this.setState({ redirect: true });
-    window.location.assign("http://kenh14.vn");
-  };
-  render() {
+  componentDidMount() {
     firebase
       .database()
       .ref()
@@ -26,15 +22,62 @@ class App extends React.Component {
       .then(snapshot => {
         console.log(snapshot.val(), "snapshot");
       });
+  }
+  onRedirect = () => {
+    window.location.assign(this.state.url);
+  };
+  onChange = event => {
+    this.setState({ url: event.target.value });
+  };
+  getURL = () => {
+    if (this.state.url) {
+      try {
+        var url = new URL(this.state.url);
+        console.log(url.protocol, url.hostname, "asdsddasda");
+        return "ABCD";
+      } catch (e) {
+        return "Error";
+      }
+    }
+    return "";
+  };
+  render() {
+    console.log(this.getURL());
     return (
-      <Row className="App">
+      <Row
+        className="App"
+        type="flex"
+        justify="center"
+        align="middle"
+        style={{ height: "100vh" }}
+      >
         <Col span={24}>
-          <header className="App-header">
-            <Text type="warning">Click the button below to redirect</Text>
-            <Button onClick={this.onRedirect} type="primary">
-              Redirect
-            </Button>
-          </header>
+          <Row type="flex" justify="center" gutter={[16, 16]}>
+            <Col span={12} gutter={[16, 16]}>
+              <Row type="flex" justify="center" gutter={[16, 16]}>
+                <Title type="warning">
+                  Paste the input url to the textbox below
+                </Title>
+              </Row>
+              <Row type="flex" justify="center" gutter={[16, 16]}>
+                <Col span={12}>
+                  <Input placeholder="Link" onChange={this.onChange} />
+                </Col>
+              </Row>
+              <Row type="flex" justify="center" gutter={[16, 16]}>
+                <Col span={24}>
+                  <Button onClick={this.onRedirect} type="primary">
+                    Redirect
+                  </Button>
+                </Col>
+              </Row>
+              <Row type="flex" justify="center" gutter={[16, 16]}>
+                <Col span={24}>
+                  <Text type="primary"></Text>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
         </Col>
       </Row>
     );
