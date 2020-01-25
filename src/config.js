734a95133f4f921e62fbb5ad67ea2662fd51324d
Thông <nguyentrthong95@gmail.com>
2020-01-25 15:50:02 +0700

    Fix database url bug

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
     apiKey: process.env.REACT_APP_APIKEY,
     authDomain: process.env.REACT_APP_AUTHDOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASEURL,
+    databaseURL:process.env.REACT_APP_DATABASEURL,
     projectId: process.env.REACT_APP_PROJECTID,
     storageBucket: process.env.REACT_APP_STORAGEBUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
