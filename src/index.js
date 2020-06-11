b79a743f859fa9c8b8be9c72785894b69bc63ac3
Thong Nguyen <thong.nt@teko.vn>
2020-06-11 13:17:34 +0700

    Remove logrocket (#removed)

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import LogRocket from "logrocket";
-LogRocket.init("hyhpik/short");
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
